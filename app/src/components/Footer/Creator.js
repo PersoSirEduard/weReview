596ba4620cb60023b15f8b8cd6794c3f02025319
michelleokolie <michelleokolie11@gmail.com>
2021-08-28 04:11:31 -0500

    created basic home page (no styling)

---
+++ app/src/components/Footer/Creator.js
@@ -0,0 +1,9 @@
+const Creator = ({username}) => {
+    return (
+        <span> 
+            {" " + username}
+        </span>
+    )
+}
+
+export default Creator
---
+++ app/src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import Creator from "./Creator"
+
+const Footer = () => {
+    return (
+        <span>
+            <p>
+            Created by
+            </p>
+           <Creator username="Bonk" />
+           <Creator username="Bonk" />
+           <Creator username="Bonk" />
+        </span>
+        
+    )
+}
+
+export default Footer
